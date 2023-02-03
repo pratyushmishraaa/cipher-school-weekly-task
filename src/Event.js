6e4fef51573608709b9034572ef5b0c80b36350d
pratyushmishraaa <pratyushmishra287@gmail.com>
2023-02-03 14:33:51 +0530

    first commit

---
+++ src/Event.js
@@ -0,0 +1,22 @@
+// import React, {Component} from 'react';
+
+// class name extends Component {
+
+   // state = {name : "Pratyush Mishra"};
+   // setName(name) {
+   //    this.setState({...this.state, name});
+   // }
+   // setName(e) {
+   //    this.setState({...this.state, "Vikas"});
+   // }
+   
+   
+   // render() {
+   //    return(<h1>The name is : {this .state.name}</h1>)
+   //    <button onClick={this.setName("Vikas")}>Click Me!</button>
+     
+     
+      
+   //    <button onClick={this.setName}>Click Me!</button>
+// }
+// }   
